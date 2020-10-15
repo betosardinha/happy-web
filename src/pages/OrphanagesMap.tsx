80490481076641d9fb776e192d012b27aa6b1abd
betosardinha <alberto.sardinha@redealumni.com>
2020-10-14 22:41:08 -0300

    update map page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -8,7 +8,6 @@ import 'leaflet/dist/leaflet.css';
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css'
-import { tileLayer } from 'leaflet';
 
 function OrphanagesMap() {
   return (
